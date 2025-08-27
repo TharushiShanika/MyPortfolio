ef2528431c5bd07b41f0af5f1775aae8eb53a558
TharushiShanika <shanika2322@gmail.com>
2025-08-27 16:48:24 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,13 @@
-
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
 import './index.css';
-import 'tailwindcss/tailwind.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
